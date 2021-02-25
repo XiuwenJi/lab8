2b82030a496ed2b311590ef28c5bbaa670d29a72
XiuwenJi <xiji@ucsd.edu>
2021-02-25 04:22:11 -0800

    thirdpartyAPI lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,5 +1,7 @@
 // 'use strict';
 
+//const e = require("express");
+
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
   initializePage();
@@ -10,18 +12,41 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Page ready");
- 	// initCamera();
- 	// initMap();
+ 	initCamera();
+ 	initMap();
  	initGestures();
  	initRSVPForm();
 }
 
 // init jQuery gestures  
 function initGestures() {
-	// add gestures listener here
+	//add gestures listener here
+	$(function(){
+		$(".judge-img").bind("taphold", tapholdHandler);
+
+		function tapholdHandler(event){
+			//get the id of the event source
+			var targetIDPrefix = event.target.id;
+			console.log("got prefix: " + targetIDPrefix );
+			//show bio
+			$("#" + targetIDPrefix + "-bio").show();
+		}
+	});
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
   // add your code here
+  $('#rsvpForm').submit(function(e){
+	  e.preventDefault();
+	  console.log("submitting form...")
+	  var rsvpEmail = $('#rsvpEmail').val();
+
+	  $.post('addRSVP', { rsvpEmail: rsvpEmail}, postCallback);
+  });
+
+  function postCallback(response){
+	  alert("RSVP form successfully submitted!");
+	  $('#rsvpEmail').val('');
+  }
 }
